5ffa194155beff13fabbea67143a56a30764b068
Deividi Paganini <dpaganini@taginfraestrutura.com.br>
2021-12-09 16:50:55 -0300

    create draft

---
+++ Dojo.01/Dojo.01.Test/Class01.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Dojo._01.Test
+{
+    public class Class1
+    {
+        [Fact]
+        Given_Jogada_When_Player1_Pedra_Player2_Pedra_Then_Empate()
+        {
+            //arrange
+            var jogador1 = "pedra";
+
+            //Act
+            //assert
+
+
+        }
+    }
+}
