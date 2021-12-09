b2bf72eec6c4072df9a56b74cf05261656aac926
Deividi Paganini <dpaganini@taginfraestrutura.com.br>
2021-12-09 17:22:51 -0300

    create new scenario

--- Dojo.01/Dojo.01.Test/Class01.cs
+++ Dojo.01/Dojo.01.Test/Class01.cs
@@ -1,19 +1,61 @@
 ﻿using System;
+using Xunit;
 
 namespace Dojo._01.Test
 {
     public class Class1
     {
+
         [Fact]
-        Given_Jogada_When_Player1_Pedra_Player2_Pedra_Then_Empate()
+        public void Given_Jogada_When_Player1_Pedra_Player2_Pedra_Then_Empate()
         {
-            //arrange
+            //Arrange
             var jogador1 = "pedra";
+            var jogador2 = "pedra";
 
             //Act
-            //assert
+            var result = Jogar.Start(jogador1, jogador2);
 
+            //Assert
+            Assert.Equal("Foi um empate", result);
+        }
 
+        [Fact]
+        public void Given_Jogar_When_Player1_Pedra_Player2_Tesoura_Then_Player1Ganha()
+        {
+            //Arrange
+            var jogador1 = "pedra";
+            var jogador2 = "tesoura";
+
+            //Act
+            var result = Jogar.Start(jogador1, jogador2);
+            
+                //Assert
+                Assert.Equal("Pedra venceu ! ", result);
+        }
+
+        [Fact]
+        public void Given_Jogar_When_Player1_Pedra_Player2_Papel_Then_Player2Ganha()
+        {
+            //Arrange
+            var jogador1 = "pedra";
+            var jogador2 = "papel";
+
+            //Act
+            var result = Jogar.Start(jogador1, jogador2);
+
+            //Assert
+            Assert.Equal("Papel venceu!", result);
         }
     }
+
+
+
+
+
+
+
+
+
+
 }
---
+++ Dojo.01/Dojo.01/Jogar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dojo._01
+{
+   public static class Jogar
+    {
+
+        public static string Start(string jogada1, string jogada2)
+        {
+            if (jogada1 == jogada2)
+            {
+                return "Foi um empate";
+            }
+
+            if (jogada1 == "pedra" && jogada2 == "tesoura")
+            {
+                return "Pedra venceu ! ";
+            }
+
+            if (jogada1 == "pedra" && jogada2 == "papel")
+            {
+                return "Pedra venceu ! ";
+            }
+
+
+            throw new NotImplementedException("Jogada nao implementada");
+        }
+
+
+
+
+
+
+
+
+    }
+}
